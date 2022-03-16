4eb72ec310d4f3fe8dda0b14272ea50f01a84764
qkwn44 <qkwn44@gmail.com>
2022-03-17 01:38:42 +0900

    2.4 login

--- src/components/App.js
+++ src/components/App.js
@@ -2,14 +2,30 @@ import React, { useState } from "react";
 import { Router } from "react-router-dom";
 import AppRouter from "components/Router";
 import { authService } from "fbase";
+import { useEffect } from "react";
 
 function App() {
   //유저의 로그인 여부 파악
-  const [isLoggedIn, setIsLoggedIn] = useState(authService.currentUse);
+  const [init, setInit] = useState(false);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  console.log(authService.currentUser);
+  useEffect(() => {
+    //사용자의 로그인 상태의 변화를 관찰하는 관찰자 추가, 이벤트리스너를 가지고 유저 상태에 변화가 있을 때 그 변화 감지
+    authService.onAuthStateChanged((user) => {
+      if (user) {
+        setIsLoggedIn(true);
+      } else {
+        setIsLoggedIn(false);
+      }
+      setInit(true);
+      //만약 init이 false라면 router를 숨기게 됨
+    });
+  }, []);
   //props로 전달 , router로 전달
   return (
     <>
-      <AppRouter isLoggedIn={isLoggedIn} />
+      {/* 만약 초기화 되면 router(login됨) or initalizing */}
+      {init ? <AppRouter isLoggedIn={isLoggedIn} /> : "initalizing..."}
       <footer>&copy; {new Date().getFullYear()} Nwitter </footer>
     </>
   );
--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -1,9 +1,12 @@
 import React from "react";
 import { useState } from "react";
+import { authService } from "../fbase";
 
 const Auth = () => {
-  const [eamil, setEmail] = useState("");
+  const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [newAccount, setNewAccount] = useState(true);
+  const [error, setError] = useState("");
   const onChange = (event) => {
     // console.log(event.target.name);
     //input 값이 변경될 때 마다 onChange함수 호출
@@ -19,10 +22,27 @@ const Auth = () => {
       setPassword(value);
     }
   };
-  const onSubmit = (event) => {
+  const onSubmit = async (event) => {
     event.preventDefault();
+    try {
+      let data;
+      if (newAccount) {
+        //create account
+        data = await authService.createUserWithEmailAndPassword(
+          email,
+          password
+        );
+      } else {
+        //log in
+        data = await authService.signInWithEmailAndPassword(email, password);
+      }
+      console.log(data);
+    } catch (error) {
+      setError(error.message);
+    }
   };
-
+  //   newAccout의 이전 값 가져와서 그 값에 반대되는 것 리턴
+  const toggleAccount = () => setNewAccount((prev) => !prev);
   return (
     <div>
       <form onSubmit={onSubmit}>
@@ -31,7 +51,7 @@ const Auth = () => {
           type="email"
           placeholder="E-mail"
           required
-          value={eamil}
+          value={email}
           onChange={onChange}
         />
         <input
@@ -42,8 +62,13 @@ const Auth = () => {
           value={password}
           onChange={onChange}
         />
-        <input type="submit" value="Log in" />
+        {error}
+        <input type="submit" value={newAccount ? "Create Account" : "Log In"} />
       </form>
+      {/* 만약 새로운 계정이 아니라면 */}
+      <span onClick={toggleAccount}>
+        {newAccount ? "Sign in" : "Create Account"}
+      </span>
       <div>
         <button>Continue with Google</button>
         <button>Continue with Github</button>
