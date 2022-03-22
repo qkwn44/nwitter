bcc000c469ddcd776d31753583384b73848bc870
qkwn44 <qkwn44@gmail.com>
2022-03-22 20:31:22 +0900

    #3.2 getting the  Nweet

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,10 +1,32 @@
 import React from "react";
+import { useEffect } from "react";
 import { useState } from "react";
 import { dbService } from "../fbase";
 
 // export default () => <span>Home </span>;
 const Home = () => {
   const [nweet, setNweet] = useState("");
+  const [nweets, setNweets] = useState([]);
+  //async사용하기 위해 개별함수 사용
+  const getNweets = async () => {
+    //getNweets: dbService를 불러와 collection("nweets") 정보를 얻는다
+
+    const dbnweets = await dbService.collection("nweets").get();
+    dbnweets.forEach((document) => {
+      // 값 대신 함수 전달, 함수 전달 시 이전값에 접근 가능
+      const nweetObject = {
+        ...document.data(),
+        id: document.id,
+      };
+      setNweets((prev) => [nweetObject, ...prev]); //새로 작성한 트윗+이전트윗
+    });
+  };
+  //component가 mount 될때 getNweets 실행
+  //getNweets는 dbService를 불러와
+  useEffect(() => {
+    //getNweets 호출
+    getNweets();
+  }, []);
   const onSubmit = async (event) => {
     //submit 할때마다 document 생성하고    event.preventDefault();
     await dbService.collection("nweets").add({
@@ -23,6 +45,7 @@ const Home = () => {
     // setNweet(event.target.value)
     //event안에 있는 target안에 있는 value get.
   };
+  console.log(nweets);
   return (
     <div>
       <form onSubmit={onSubmit}>
@@ -36,6 +59,11 @@ const Home = () => {
         />
         <input type="submit" value="Nweet" />
       </form>
+      {nweets.map((nweet) => (
+        <div key={nweet.id}>
+          <h4>{nweet.nweet}</h4>
+        </div>
+      ))}
     </div>
   );
 };
